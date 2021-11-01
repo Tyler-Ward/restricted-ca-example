d515fb94c8fd0a96e2f3a64b3a2899ee4cb7c94e
Tyler Ward <tyler@scorpia.co.uk>
2021-11-01 00:37:57 +0000

    Update CA configuration and generation

--- 1_ca_creation/createca.sh
+++ 1_ca_creation/createca.sh
@@ -2,6 +2,6 @@
 
 set -x
 
-openssl genrsa -out rootCA.key 4069
+openssl genrsa -out rootCA.key 4096
 openssl req -x509 -new -nodes -key rootCA.key -sha512 -days 3650 -out rootCA.crt -config ca.conf
 openssl x509 -text -in rootCA.crt
